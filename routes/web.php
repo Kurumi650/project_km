add7b2351bcd77499e02b5d6c29f3a3331767b61
Kurumi650 <oooamjadkurumi@gmail.com>
2024-09-05 12:09:15 +0200

    "Routes toegevoegd voor het beheren van voertuigen en ritten, inclusief GET- en POST-routes voor de verschillende acties."

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VoertuigController;
 
-Route::get('/', function () {
-    return view('index');
-});
+// Hier zijn get route
+Route::get('/', [VoertuigController::class, 'index']);
+Route::get('/voertuig_toevogen', [VoertuigController::class, 'voertuig_toevogen']);
+Route::get('/rit_toevogen', [VoertuigController::class, 'rit_toevogen']);
+
+
+// Hier wordt post route 
+Route::post('/rit_opslaan', [VoertuigController::class, 'rit_opslaan']);
+Route::post('/voertuig_opslaan', [VoertuigController::class, 'voertuig_opslaan']);
